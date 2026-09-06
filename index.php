8fb4e9f50cfe6d9581cb813a0c4386206df568ab
Silvia <silviaa060605@gmail.com>
2026-09-06 10:49:22 +0700

    Add Book and Member classes

--- index.php
+++ index.php
@@ -7,18 +7,21 @@ require_once "DigitalBook.php";
 // Membuat beberapa object Book
 $buku1 = new Book("Laskar Pelangi", "Andrea Hirata");
 $buku2 = new Book("Bumi", "Tere Liye");
+$buku3 = new Book("Negeri 5 Menara", "Ahmad Fuadi");
 
 // Membuat object DigitalBook
 $bukuDigital = new DigitalBook("Filosofi Teras", "Henry Manampiring", 5);
 
 // Membuat object Member
 $member1 = new Member("Silvia", "M001");
+$member2 = new Member("Alya", "M002");
 
 // Menampilkan informasi buku
 echo "<h2>DAFTAR BUKU</h2>";
 
 $buku1->tampilkanInfo();
 $buku2->tampilkanInfo();
+$buku3->tampilkanInfo();
 
 // Menampilkan informasi digital book
 echo "<h2>DIGITAL BOOK</h2>";
@@ -29,6 +32,7 @@ $bukuDigital->tampilkanInfo();
 echo "<h2>INFORMASI MEMBER</h2>";
 
 $member1->tampilInfo();
+$member2->tampilInfo();
 
 // Simulasi peminjaman
 echo "<h2>SIMULASI PEMINJAMAN</h2>";
